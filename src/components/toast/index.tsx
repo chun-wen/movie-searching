c754758d8c9ff4838ee1e1f442c0169ee915441e
chunwen <hsbsbbdn@gmail.com>
2023-07-24 00:53:01 +0800

    feat: add toast component

---
+++ src/components/toast/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './style.scss';
+
+import { notification } from 'antd';
+import { ArgsProps } from 'antd/lib/notification/interface';
+import { isUndefined } from 'lodash';
+
+import errorSvg from '@/Image/error.svg';
+import successSvg from '@/Image/success.svg';
+
+type NotificationType = 'success' | 'info' | 'warning' | 'error';
+
+interface ToastProps extends ArgsProps {
+  status?: NotificationType;
+  customClass?: string;
+}
+
+const Toast = ({ description, duration, message, className, status, ...props }: ToastProps) => {
+  let suffixIcon;
+  switch (status) {
+    case 'success':
+      suffixIcon = <img className="toast__icon" src={successSvg} alt="success-svg" />;
+      break;
+    case 'error':
+      suffixIcon = <img className="toast__icon" src={errorSvg} alt="error-svg" />;
+      break;
+    default:
+      break;
+  }
+  (() => {
+    if (!isUndefined(description)) {
+      notification.open({
+        description,
+        message,
+        duration,
+        className,
+        icon: suffixIcon,
+        ...props,
+      });
+    }
+  })();
+};
+
+export default Toast;
