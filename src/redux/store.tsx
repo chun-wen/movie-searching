0372a16716785d8250bd3504bb5b47eabf66bbea
chunwen <hsbsbbdn@gmail.com>
2023-07-24 00:48:38 +0800

    feat: export store and set devTools to true

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -19,11 +19,14 @@ const reducer = combineReducers({
 })
 
 const store = configureStore({
+  devTools: true,
   reducer,
   middleware
 })
 
 sagaMiddleware.run(rootSaga);
+
+export default store;
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
