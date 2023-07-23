ee4be2e36a4f2f1e127d435017beccefba0df9b1
chunwen <hsbsbbdn@gmail.com>
2023-07-24 00:34:14 +0800

    feat: add redux store

---
+++ src/redux/store.tsx
@@ -0,0 +1,31 @@
+import { combineReducers, configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga';
+
+import rootSaga from '@/Redux/rootSaga';
+
+import movieSlice from '@/Slice/movieSlice';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const middleware = [
+  ...getDefaultMiddleware({
+    thunk: false
+  }),
+  sagaMiddleware
+];
+
+const reducer = combineReducers({
+  movie:movieSlice
+})
+
+const store = configureStore({
+  reducer,
+  middleware
+})
+
+sagaMiddleware.run(rootSaga);
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
