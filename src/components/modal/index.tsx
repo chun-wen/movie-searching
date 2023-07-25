2df8002c88092fcfc0f1f7c00451c75362355189
chunwen <hsbsbbdn@gmail.com>
2023-07-25 18:08:43 +0800

    feat: add modal component

---
+++ src/components/modal/index.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+import { Button, Modal as ModalAntd } from 'antd';
+import { ModalProps as ModalAntdProps } from 'antd/lib/modal';
+
+import { MovieInfo } from '@/interface/I_Movie';
+
+interface ModalProps extends MovieInfo {
+  open: boolean;
+  onCancel?: (values: any) => void;
+}
+
+const Modal = ({
+  original_title,
+  poster_path,
+  release_date,
+  vote_average,
+  open,
+  onCancel,
+  ...props
+}: ModalProps) => {
+  return (
+    <ModalAntd title={original_title} centered open={open} onCancel={onCancel}>
+      <p>{vote_average}</p>
+    </ModalAntd>
+  );
+};
+
+export default Modal;
