ed890791c97f22cb0e6b1105b5345633081fdc09
chunwen <hsbsbbdn@gmail.com>
2023-07-25 18:09:12 +0800

    feat: add modal component to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
-import React, { Suspense, useEffect } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 
 import { Skeleton } from 'antd';
+import { clsx } from 'clsx';
+import { isNull } from 'lodash';
 import { useRouter } from 'next/router';
 
 import Card from '@/components/card';
 import Input from '@/components/input';
+import Modal from '@/components/modal';
 import { useAppDispatch, useAppSelector } from '@/hooks';
+import { MovieInfo } from '@/interface/I_Movie';
 import { getConfiguration } from '@/redux/slice/configurationSlice';
 import { getNowPlaying, getSearchList, setSearchList } from '@/redux/slice/movieSlice';
 
@@ -14,28 +18,49 @@ export default function Home() {
   const router = useRouter();
   const { movie_nowPlayingList, isloading } = useAppSelector((state) => state.movie);
 
+  const [isOpen, setIsOpen] = useState(true);
+  const [currentModalData, setCurrentModalData] = useState<MovieInfo | null>(null);
+
   const onSearch = (value: string) => {
     dispatch(getSearchList(value));
     router.push(`/search/${value}`);
   };
 
   useEffect(() => {
-    dispatch(getConfiguration())
+    dispatch(getConfiguration());
     dispatch(getNowPlaying());
-  }, [])
+  }, []);
 
   return (
     // <Suspense fallback={<Skeleton />}>
-    <>      
-    <Input placeholder="Search for a movie..." className="" onSearch={onSearch} />
-      <div className='flex gap-4 flex-wrap pt-4 justify-center'>
-      {
-        !isloading && movie_nowPlayingList.map((movieInfo) => (
-          <Card key={movieInfo.id} {...movieInfo} />
-        ))
-      }
+    <>
+      <Input placeholder="Search for a movie..." className="" onSearch={onSearch} />
+      <div className="flex gap-4 flex-wrap pt-4 justify-center">
+        {!isloading &&
+          movie_nowPlayingList.map((movieInfo) => (
+            <div key={movieInfo.id}>
+              <Card
+                {...movieInfo}
+                onClick={() => {
+                  setIsOpen(true);
+                  setCurrentModalData(movieInfo);
+                }}
+              />
+            </div>
+          ))}
+        {/* show Modal */}
+        {!isNull(currentModalData) && (
+          <Modal
+            {...currentModalData}
+            open={isOpen}
+            onCancel={() => {
+              setCurrentModalData(null);
+              setIsOpen(false);
+            }}
+          />
+        )}
       </div>
-      </>
+    </>
     // </Suspense>
   );
 }
