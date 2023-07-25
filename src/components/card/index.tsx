c0839d3e14e514acbabee85e54b6404694ef8c5b
chunwen <hsbsbbdn@gmail.com>
2023-07-25 15:02:44 +0800

    feat: add card component and remove component scss

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,1 +1,23 @@
+import React from 'react';
 
+import { Card as CardAntd } from 'antd';
+import dayjs from 'dayjs';
+
+import { MovieInfo } from '@/interface/I_Movie';
+
+// const { Meta } = CardAntd;
+
+const Card = ({ ...props }: MovieInfo) => {
+  return (
+    <CardAntd
+      hoverable
+      style={{ width: 240 }}
+      cover={<img alt={props.original_title} src={props.poster_path} />}
+    >
+      <p className='font-bold'>{props.original_title}</p>
+      <p>{dayjs(props.release_date).format('DD/MM/YYYY')}</p>
+    </CardAntd>
+  );
+};
+
+export default Card;
