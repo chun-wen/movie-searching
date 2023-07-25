0c28ab0aef66a0f4327b6506206c51abe1bd948a
chunwen <hsbsbbdn@gmail.com>
2023-07-25 15:03:04 +0800

    feat: add input component and remove component scss

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,1 +1,18 @@
+import React from 'react';
 
+import { Input as InputAntd } from 'antd';
+import { InputProps as InputAntdProps, SearchProps } from 'antd/lib/input';
+
+const { Search } = InputAntd;
+
+interface InputProps extends InputAntdProps, SearchProps {}
+
+const Input = ({
+  placeholder = 'Search for a movie...',
+  className = '',
+  onSearch = (value: string) => undefined,
+}: InputProps) => {
+  return <Search placeholder={placeholder} className={className} onSearch={onSearch} />;
+};
+
+export default Input;
