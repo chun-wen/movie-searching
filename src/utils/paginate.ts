6997457c5ac9b1ea283fcdb3be2f9fc36ff3e797
chunwen <hsbsbbdn@gmail.com>
2023-07-27 01:54:59 +0800

    feat: add data function utils

---
+++ src/utils/paginate.ts
@@ -0,0 +1,20 @@
+import { MovieInfo } from '@/interface/I_Movie';
+
+const paginateArray = ({
+  data,
+  pageSize,
+  pageNumber,
+}: {
+  data: MovieInfo[];
+  pageSize: number;
+  pageNumber: number;
+}) => {
+  const startIndex = (pageNumber - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+  return data.slice(startIndex, endIndex);
+};
+
+
+
+
+export default paginateArray;
---
+++ src/utils/sorting.ts
@@ -0,0 +1,29 @@
+import { SortOrder as SortOrderProps } from 'antd/lib/table/interface';
+
+import { MovieInfo, SortOrder } from '@/interface/I_Movie';
+
+const sortingArray = ({
+  data,
+  order,
+  column,
+}: {
+  data: MovieInfo[];
+  order?: SortOrderProps;
+  column: keyof MovieInfo;
+}) => {
+  const result = data.sort((a, b) => {
+    const valueA = a[column];
+    const valueB = b[column];
+
+    if (typeof valueA === 'number' && typeof valueB === 'number') {
+      return order === SortOrder.Asc ? valueA - valueB : valueB - valueA;
+    } else {
+      // Handle non-numeric properties or any other custom logic here
+      return 0;
+    }
+  });
+
+  return result;
+};
+
+export default sortingArray;
