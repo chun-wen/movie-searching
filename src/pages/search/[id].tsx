48b028d329582337f9eaf94ddb85d70d2862cd83
chunwen <hsbsbbdn@gmail.com>
2023-07-27 01:54:36 +0800

    feat: add page dispatch methods

--- src/pages/search/[id].tsx
+++ src/pages/search/[id].tsx
@@ -1,47 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import dayjs from 'dayjs';
 
 import Table from '@/components/table';
 import { useAppDispatch, useAppSelector } from '@/hooks';
-import { setSearchList } from '@/redux/slice/movieSlice';
+import { filterSearchList, setSearchList } from '@/redux/slice/movieSlice';
 
 const SearchPage = () => {
   const dispatch = useAppDispatch();
   const { movieData, searchList } = useAppSelector((state) => state.movie);
-  console.log({movieData, searchList})
+  const {images:{secure_base_url, poster_sizes}} = useAppSelector((state) => state.configuration);
+  const [currentPage, setCurrentPage] = useState(1);
 
   const columns = [
-    { title: 'Image', dataIndex: 'image', render: (data:string) => <img src={data} />},
-    { title: 'Title', dataIndex: 'title'},
-    { title: 'Language', dataIndex: 'language'},
-    { title: 'Overview', dataIndex: 'overview'},
-    { title: 'Popularity', dataIndex: 'popularity'},
-    { title: 'Release', dataIndex: 'release', render: (date:Date) => <p>{dayjs(date).format('DD/MM/YYYY')}</p>},
-  ]
+    {
+      title: 'Image',
+      dataIndex: 'image',
+      render: ({
+        poster_path,
+        original_title,
+      }: {
+        poster_path: string;
+        original_title: string;
+      }) => <img src={`${secure_base_url}/${poster_sizes[4]}/${poster_path}`} alt={original_title} />,
+    },
+    { title: 'Title', dataIndex: 'original_title' },
+    { title: 'Language', dataIndex: 'original_language' },
+    { title: 'Overview', dataIndex: 'overview', sorter: true },
+    { title: 'Popularity', dataIndex: 'popularity', sorter: true },
+    {
+      title: 'Release',
+      dataIndex: 'release_date',
+      render: (date: Date) => <p>{dayjs(date).format('DD/MM/YYYY')}</p>,
+      sorter: true,
+    },
+  ];
 
   const dataSource = searchList.map((movie) => {
-    const {poster_path, original_title,release_date,overview, popularity} = movie;
-    return ({
+    const { poster_path, original_title, overview, popularity,original_language } = movie;
+    return {
       ...movie,
-      image: poster_path,
-      title:original_title,
-      release:release_date,
+      image: { poster_path, original_title },
+      original_language,
+      original_title,
       overview,
-      popularity
-    })
-  })
+      popularity,
+    };
+  });
 
   return (
     <>
-       <h1>Search Results</h1>
-       <Table dataSource={dataSource} columns={columns} onChange={(pagination, filters, sorter, extra) => {
-        if(extra.action === 'sort'){
-          dispatch(setSearchList())
-        }
-       }}/>
+      <h1>Search Results</h1>
+      <Table
+        dataSource={dataSource}
+        columns={columns}
+        onChange={(pagination, filters, sorter, extra) => {
+          if (extra.action === 'sort' && !sorter?.order) {
+            console.log(sorter);
+            dispatch(filterSearchList({
+              order: sorter.order,
+              column: sorter.column,
+              currentPage
+            }));
+          }
+          if(extra.action === 'paginate'){
+            dispatch(filterSearchList({
+              order: sorter.order,
+              column: sorter.column,
+              currentPage
+            }));
+          }
+        }}
+        pagination={{
+          current: currentPage,
+          total: movieData.total_results,
+          showTotal: (total, range) => `${range[0]}-${range[1]} of ${total} items`,
+          defaultPageSize: 20,
+          showQuickJumper: false
+        }}
+      />
     </>
-  )
-}
+  );
+};
 
 export default SearchPage;
