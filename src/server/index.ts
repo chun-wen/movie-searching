545e5e5618414bf269b6b63e2243311f3a1e2484
chunwen <hsbsbbdn@gmail.com>
2023-07-28 04:03:00 +0800

    feat: adjust timeout time to temporary fix ECONNABORTED issue

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from 'axios';
 
-import Toast from '@/components/toast';
+import Toast from '@/Components/toast';
 
 const token =
   'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMTg4Mzk0ZTk1YjMwZTI1NjE5MjFlN2MyOWVlZmI5NSIsInN1YiI6IjY0YmJhMTgwNThlZmQzMDEzOTUzMDE1MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2KBP-Wb6GNGwDdlsA8pDqGk76oaBepBPNERpwSUp1oE';
@@ -10,7 +10,7 @@ export class BasicService {
   constructor() {
     this.fetcher = axios.create({
       baseURL: 'https://api.themoviedb.org/3',
-      timeout: 1000,
+      timeout: 3000,
       headers: {
         accept: 'application/json',
         Authorization: `Bearer ${token}`,
@@ -25,12 +25,13 @@ export class BasicService {
         return response;
       },
       function (error) {
-        if (error.status_code === 7) {
-          Toast({
-            message: error.status_message,
-            status: 'error',
-          });
-        }
+        console.log(error);
+        // if (error.status_code === 7) {
+        Toast({
+          message: error.status_message,
+          status: 'error',
+        });
+        // }
         // Any status codes that falls outside the range of 2xx cause this function to trigger
         // Do something with response error
         return Promise.reject(error);
