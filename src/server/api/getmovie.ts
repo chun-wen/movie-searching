88e637f530d97d4e2af1cdedad032c98adf833a0
chunwen <hsbsbbdn@gmail.com>
2023-07-27 23:56:46 +0800

    feat: add movie credit、reviews、details api

--- src/server/api/getmovie.ts
+++ src/server/api/getmovie.ts
@@ -5,6 +5,10 @@ import { BasicService } from '../index';
 class GetMovieApi extends BasicService {
   getSearchList = (query: string) => this.fetcher.get(`/search/movie?query=${query}`);
   getNowPlaying = () => this.fetcher.get('/movie/now_playing');
+  getMovieDetails = (id: string) => this.fetcher.get(`/movie/${id}`);
+  getMovieReviews = (id: string) => this.fetcher.get(`/movie/${id}/reviews`);
+  getMovieCredits = (id: string, page: number) =>
+    this.fetcher.get(`/movie/${id}/credits?page=${page}`);
 }
 
 const getMovie = new GetMovieApi();
