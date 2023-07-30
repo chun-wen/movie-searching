dd00bc55731abd59c13564b19a86691429467738
chunwen <hsbsbbdn@gmail.com>
2023-07-30 11:08:55 +0800

    chore: add image.tmdb domain

--- next.config.js
+++ next.config.js
@@ -10,6 +10,11 @@ const nextConfig = {
         hostname: 'images.unsplash.com',
         port: '',
       },
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        port: '',
+      },
     ],
   },
   // sassOptions: {
