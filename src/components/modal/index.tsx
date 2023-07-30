3e96b5fabf82a29bcb597c0e339a927ffba972bf
chunwen <hsbsbbdn@gmail.com>
2023-07-30 11:16:58 +0800

    feat: adjust detail modal component(not finished)

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -1,28 +1,82 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
-import { Button, Modal as ModalAntd } from 'antd';
+import { Button, Col, Layout, Modal as ModalAntd, Row } from 'antd';
 import { ModalProps as ModalAntdProps } from 'antd/lib/modal';
+import { isEmpty } from 'lodash';
+import Image from 'next/image';
 
 import { MovieInfo } from '@/Interface/I_MovieGeneral';
 
-interface ModalProps extends MovieInfo {
+import { useAppDispatch, useAppSelector } from '@/hooks';
+import { getMovieDetail } from '@/redux/slices/movieSlice';
+
+import Card from '../card';
+
+interface ModalProps extends Pick<MovieInfo, 'id'> {
   open: boolean;
   onCancel?: (values: any) => void;
 }
+const { Content } = Layout;
 
-const Modal = ({
-  original_title,
-  poster_path,
-  release_date,
-  vote_average,
-  open,
-  onCancel,
-  ...props
-}: ModalProps) => {
+const Modal = ({ id, open, onCancel }: ModalProps) => {
+  const dispatch = useAppDispatch();
+  const {
+    movieDetail: { movieInfo, movieCast, movieReview },
+  } = useAppSelector((state) => state.movie);
+  
+  const isLoading = isEmpty(movieInfo) || isEmpty(movieReview);
+
+  const handleLoadMore = () => {};
+
+  useEffect(() => {
+    dispatch(getMovieDetail({ id }));
+  }, []);
+
+  console.log('1', movieInfo, movieCast, movieReview);
+
   return (
-    <ModalAntd title={original_title} centered open={open} onCancel={onCancel}>
-      <p>{vote_average}</p>
-    </ModalAntd>
+    !isLoading && (
+      <ModalAntd centered open={open} onCancel={onCancel} footer={null}>
+        <Layout>
+          <Content style={{ padding: '24px' }}>
+            <Row gutter={[16, 16]}>
+              <Col xs={24} sm={8}>
+                <Image src={movieInfo.poster_path} alt="Movie Poster" width={500}
+      height={500}/>
+              </Col>
+              <Col xs={24} sm={16}>
+                <div>Title</div>
+                <div>Score</div>
+                <div>Description</div>
+                <button>Favorite</button>
+              </Col>
+            </Row>
+
+            {/* Section 2 */}
+            <div style={{ overflowX: 'auto' }}>
+              {/* <Card>
+         </Card> */}
+            </div>
+
+            {/* Section 3 */}
+            <InfiniteScroll
+              dataLength={movieReview.total_results}
+              next={handleLoadMore}
+              hasMore
+              loader={<div>Loading...</div>}
+              style={{ width: '100%' }}
+            >
+              {movieReview?.results?.map((comment) => (
+                <div key={comment.id} style={{ width: '100%' }}>
+                  {comment.content}
+                </div>
+              ))}
+            </InfiniteScroll>
+          </Content>
+        </Layout>
+      </ModalAntd>
+    )
   );
 };
 
