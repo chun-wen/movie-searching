f87b477b0103ec584c1e01515987e6cd79cd57cb
chunwen <hsbsbbdn@gmail.com>
2023-07-30 11:14:21 +0800

    feat: adjust ui and remove logo

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -12,14 +12,14 @@ export default function Layout({ children }: any) {
   return (
     <>
       <Header
-        className="bg-bg-lightGray flex px-4 justify-between "
+        className="bg-bg-lightGray flex px-4 justify-end"
       >
-        <Image
+        {/* <Image
           width={64}
           height={64}
           src="https://images.unsplash.com/photo-1478720568477-152d9b164e26?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80"
           alt="logo"
-        />
+        /> */}
         <Menu />
       </Header>
       <main className="p-4 flex-1">{children}</main>
