c5d49a38e842bd106242512ab57277ee104f853f
chunwen <hsbsbbdn@gmail.com>
2023-07-30 14:48:16 +0800

    feat: adjust header part info and style

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -1,17 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
 
-import { Button, Col, Layout, Modal as ModalAntd, Row } from 'antd';
+import { CommentOutlined } from '@ant-design/icons';
+import { Button, Col, Layout, Modal as ModalAntd, Row, Tooltip } from 'antd';
 import { ModalProps as ModalAntdProps } from 'antd/lib/modal';
+import dayjs from 'dayjs';
 import { isEmpty } from 'lodash';
 import Image from 'next/image';
 
 import { MovieInfo } from '@/Interface/I_MovieGeneral';
 
 import { useAppDispatch, useAppSelector } from '@/hooks';
+import { Department } from '@/interface/I_MovieCredit';
 import { getMovieDetail } from '@/redux/slices/movieSlice';
+import { setCollectionMovie } from '@/redux/slices/userSlice';
 
 import Card from '../card';
+import WatchList from '../watchlist';
 
 interface ModalProps extends Pick<MovieInfo, 'id'> {
   open: boolean;
@@ -22,43 +27,74 @@ const { Content } = Layout;
 const Modal = ({ id, open, onCancel }: ModalProps) => {
   const dispatch = useAppDispatch();
   const {
-    movieDetail: { movieInfo, movieCast, movieReview },
+    movieDetail: { movieInfo, movieCrew, movieReview },
   } = useAppSelector((state) => state.movie);
-  
+
   const isLoading = isEmpty(movieInfo) || isEmpty(movieReview);
 
+  const [isCollet, setIsCollect] = useState(false);
   const handleLoadMore = () => {};
 
   useEffect(() => {
     dispatch(getMovieDetail({ id }));
   }, []);
 
-  console.log('1', movieInfo, movieCast, movieReview);
+  const { poster_path, title, vote_average, release_date, genres, overview } = movieInfo;
+  console.log('1', movieInfo, movieCrew, movieReview);
 
   return (
     !isLoading && (
-      <ModalAntd centered open={open} onCancel={onCancel} footer={null}>
+      <ModalAntd centered open={open} onCancel={onCancel} footer={null} width={'auto'}>
         <Layout>
           <Content style={{ padding: '24px' }}>
             <Row gutter={[16, 16]}>
               <Col xs={24} sm={8}>
-                <Image src={movieInfo.poster_path} alt="Movie Poster" width={500}
-      height={500}/>
+                <Image src={poster_path} alt="Movie Poster" width={350} height={350} />
               </Col>
               <Col xs={24} sm={16}>
-                <div>Title</div>
-                <div>Score</div>
-                <div>Description</div>
-                <button>Favorite</button>
+                <div className="header-item">
+                  <h2 className="header-item--title"> {title}</h2>
+                  <div className="flex flex-row items-center">
+                    <p className="pr-2">Release Date:</p>
+                    <p>{dayjs(release_date).format('DD/MM/YYYY')}</p>
+                  </div>
+                </div>
+                <div className="header-item">
+                <h3 className="header-item--subtitle">Score</h3>
+                  <div className="flex flex-row items-center gap-3">
+                    <div className="flex flex-row items-center">
+                      <p className="pr-2">Score:</p>
+                      <span>{vote_average}</span>
+                    </div>
+                    <WatchList
+                      isColletProps={false}
+                      onCollect={(movieInfo) => {
+                        dispatch(setCollectionMovie(movieInfo));
+                      }}
+                    />
+                  </div>
+                </div>
+                <div className="header-item">
+                  {/* description part */}
+                  <div>
+                    <h3 className="header-item--subtitle">Overview</h3>
+                    <p>{overview}</p>
+                  </div>
+                </div>
+                <div className="header-item">
+                  <h3 className="header-item--subtitle">Director</h3>
+                  <p>
+                    {movieCrew.find((cast) => cast?.department === Department.Directing)?.name ||
+                      ''}
+                  </p>
+                </div>
               </Col>
             </Row>
-
-            {/* Section 2 */}
+            ∏{/* Section 2 */}
             <div style={{ overflowX: 'auto' }}>
               {/* <Card>
          </Card> */}
             </div>
-
             {/* Section 3 */}
             <InfiniteScroll
               dataLength={movieReview.total_results}
