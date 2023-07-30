a5b96f20c41c8eb45e4a4b0426c7a9acaecd5fa0
chunwen <hsbsbbdn@gmail.com>
2023-07-31 01:22:29 +0800

    fix: typescript error

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -7,8 +7,46 @@ declare module '*.svg' {
 }
 
 declare module '@lucky-canvas/react' {
-import React from 'react';
+  export interface Block {
+    padding: string;
+    background: string;
+    [key: string]: any;
+  }
 
-    // Declare the LuckyWheel component here
-  export const LuckyWheel: React.FC<JSX.Element>;
+  export interface Prizes {
+    prizes:{
+      range? : number;
+      fonts:{
+        text: string;
+      }[]
+    }[]
+    background: string;
+  }
+
+  export interface Buttons {
+    buttons:{
+      radius: number | string;
+      pointer?: boolean;
+      background?: string;
+      fonts?:{
+        text: string;
+        top?: string | number;
+      }[]
+    }[];
+  }
+
+  export interface LuckyWheelProps {
+    // Define the LuckyWheelProps type based on your needs
+    width: string;
+    height: string;
+    blocks: Block[];
+    prizes: Prize[];
+    buttons: Button[];
+    onStart: () => void;
+    onEnd: (prize: any) => void;
+    ref: any;
+  }
+
+  export const LuckyWheel: React.FC<LuckyWheelProps>;
 }
+
