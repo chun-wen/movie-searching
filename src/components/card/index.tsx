505f6a6eb60085915cec2b40cd9cd062abb8f62b
chunwen <hsbsbbdn@gmail.com>
2023-07-30 22:18:15 +0800

    feat: adjust card component

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,12 +1,15 @@
 import React, { useMemo, useState } from 'react';
 
-import { MinusOutlined, PlusOutlined } from '@ant-design/icons';
 import { Button, Card as CardAntd, Space, Tooltip } from 'antd';
 import dayjs from 'dayjs';
 
 import { MovieInfo } from '@/Interface/I_MovieGeneral';
 
+import { useAppDispatch } from '@/hooks';
 import { Cast } from '@/interface/I_MovieCredit';
+import { setCollectionMovie } from '@/redux/slices/userSlice';
+
+import WatchList from '../watchlist';
 
 export enum CardEnum {
   Normal = 'Normal',
@@ -19,28 +22,25 @@ interface CardProps {
   onCollect?: (movieInfo: MovieInfo) => void;
   cardType?: CardType;
   movieInfo?: MovieInfo;
-  cast?: Pick<Cast, 'name' | 'profile_path' | 'popularity' | 'id'>
+  cast?: Pick<Cast, 'name' | 'profile_path' | 'popularity' | 'id'>;
 }
 
-const Card = ({
-  onClick,
-  onCollect,
-  cardType = CardEnum.ModalCard,
-  ...props
-}: CardProps) => {
-  const [isCollet, setIsCollect] = useState(false);
-  
+const Card = ({ onClick, onCollect, cardType = CardEnum.ModalCard, ...props }: CardProps) => {
+  const dispatch = useAppDispatch();
+  const [isCollet, setIsCollect] = useState(props.movieInfo?.isCollet || false);
+
   const renderCard = useMemo(() => {
-    console.log('1');
-    switch(cardType){
-      case CardEnum.Normal: 
-         return (<CardAntd
-          hoverable={false}
-          style={{ width: 300, height: 460 }}
-          cover={<img alt={props?.cast?.name || ''} src={props?.cast?.profile_path || ''} />}
-        >
-          <p className="font-bold text-2xl break-words">{props?.cast?.name}</p>
-        </CardAntd>)
+    switch (cardType) {
+      case CardEnum.Normal:
+        return (
+          <CardAntd
+            hoverable={false}
+            style={{ width: 300, height: 460 }}
+            cover={<img alt={props?.cast?.name || ''} src={props?.cast?.profile_path || ''} />}
+          >
+            <p className="font-bold text-2xl break-words">{props?.cast?.name}</p>
+          </CardAntd>
+        );
       case CardEnum.ModalCard:
       default:
         return (
@@ -48,7 +48,9 @@ const Card = ({
             <CardAntd
               hoverable
               style={{ width: 240, height: 460 }}
-              cover={<img alt={props?.movieInfo?.original_title} src={props?.movieInfo?.poster_path} />}
+              cover={
+                <img alt={props?.movieInfo?.original_title} src={props?.movieInfo?.poster_path} />
+              }
             >
               <p className="font-bold">{props?.movieInfo?.original_title}</p>
               <p>{dayjs(props?.movieInfo?.release_date).format('DD/MM/YYYY')}</p>
@@ -60,28 +62,21 @@ const Card = ({
               className="card-item"
             >
               <div className="flex justify-end">
-                <Tooltip title={isCollet ? 'remove from watch list': 'add to watch list'}>
-                  <Button
-                    className='card-item__button'
-                    icon={isCollet ? <MinusOutlined /> : <PlusOutlined />}
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      onCollect?.({ ...props?.movieInfo as MovieInfo });
-                      setIsCollect(!isCollet);
-                    }}
-                  />
-                </Tooltip>
+                <WatchList
+                  isColletProps={isCollet}
+                  onCollect={() => {
+                    dispatch(setCollectionMovie(props.movieInfo as MovieInfo));
+                    setIsCollect(!isCollet);
+                  }}
+                />
               </div>
             </CardAntd>
           </div>
-        )
+        );
     }
-  },[isCollet, props])
-  
+  }, []);
 
-  return (
-    renderCard
-  )
+  return renderCard;
 };
 
 export default Card;
