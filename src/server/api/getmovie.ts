bb1fde25bebff79c45591eb9a6e1898b95d08c21
chunwen <hsbsbbdn@gmail.com>
2023-07-30 11:18:29 +0800

    feat: adjust api parameter type

--- src/server/api/getmovie.ts
+++ src/server/api/getmovie.ts
@@ -6,9 +6,9 @@ class GetMovieApi extends BasicService {
   getSearchList = (params: { query: string; page: number }) =>
     this.fetcher.get(`/search/movie`, { params });
   getNowPlaying = () => this.fetcher.get('/movie/now_playing');
-  getMovieDetails = (id: string) => this.fetcher.get(`/movie/${id}`);
-  getMovieReviews = (id: string) => this.fetcher.get(`/movie/${id}/reviews`);
-  getMovieCredits = (id: string, page: number) =>
+  getMovieDetails = (id: number) => this.fetcher.get(`/movie/${id}`);
+  getMovieReviews = (id: number) => this.fetcher.get(`/movie/${id}/reviews`);
+  getMovieCredits = (id: number, page : number = 1) =>
     this.fetcher.get(`/movie/${id}/credits?page=${page}`);
 }
 
