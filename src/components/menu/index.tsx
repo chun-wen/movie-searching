d7edfc667e53a869a62cb07ec6802c4e236c8da8
chunwen <hsbsbbdn@gmail.com>
2023-07-30 11:13:54 +0800

    feat: fix router wrong path and adjust css

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
 import { MenuOutlined } from '@ant-design/icons';
-import { Dropdown, Menu as MenuAntd } from 'antd';
+import { Dropdown, Menu as MenuAntd, MenuProps } from 'antd';
 import Link from 'next/link';
 
-const menuItems = [
+const menuItems: MenuProps['items'] = [
   { label: <Link href="/">Home</Link>, key: 'home' },
-  { label: <Link href="/booking">Booking</Link>, key: 'booking' },
+  { label: <Link href="/collect">Watching List</Link>, key: 'collect' },
 ];
 
-const Menu = ({ className }: any) => {
+const Menu = () => {
   return (
     <>
       <MenuAntd mode="horizontal" className={`hidden sm:flex`} items={menuItems} />
-      <Dropdown menu={{ items: menuItems }} className="visible pr-2 sm:hidden">
+      <Dropdown menu={{ items: menuItems }} className="flex p-2 sm:hidden">
         <MenuOutlined style={{ fontSize: '32px'}} />
       </Dropdown>
     </>
